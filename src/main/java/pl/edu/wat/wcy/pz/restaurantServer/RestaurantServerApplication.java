8a3d0cc2c7f2d662cdc1a74ee6d1c0ef0f204644
unknown <adziomix@gmail.com>
2018-12-22 23:33:35 +0100

    Base project and database connection with Hibernate

---
+++ src/main/java/pl/edu/wat/wcy/pz/restaurantServer/RestaurantServerApplication.java
@@ -0,0 +1,14 @@
+package pl.edu.wat.wcy.pz.restaurantServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantServerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/pl/edu/wat/wcy/pz/restaurantServer/RestaurantServerApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.edu.wat.wcy.pz.restaurantServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
